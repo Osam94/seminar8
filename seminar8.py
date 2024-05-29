3f95d3135be5add2808f284ce1ffe8d41d6f0277
osamah <asm222299@mail.ru>
2024-05-29 16:14:37 +0300

    код готов к работы

---
+++ seminar8.py
@@ -0,0 +1,73 @@
+def work_with_phonebook():
+    choice = show_menu()
+    phone_book = read_txt('phonebook.txt')
+
+    while choice != 6:
+        if choice == 1:
+            print_result(phone_book)
+        elif choice == 2:
+            last_name = input('Введите фамилию: ')
+            print(find_by_lastname(phone_book, last_name))
+        elif choice == 3:
+            number = input('Введите номер телефона: ')
+            print(find_by_number(phone_book, number))
+        elif choice == 4:
+            user_data = input('Введите новые данные (Фамилия,Имя,Телефон,Описание): ')
+            add_user(phone_book, user_data)
+        elif choice == 5:
+            write_txt('phonebook.txt', phone_book)
+        choice = show_menu()
+
+def show_menu():
+    print("\nВыберите необходимое действие:\n"
+          "1. Отобразить весь справочник\n"
+          "2. Найти абонента по фамилии\n"
+          "3. Найти абонента по номеру телефона\n"
+          "4. Добавить абонента в справочник\n"
+          "5. Сохранить справочник в текстовом формате\n"
+          "6. Закончить работу")
+    choice = int(input("Введите номер действия: "))
+    return choice
+
+def read_txt(filename):
+    phone_book = []
+    fields = ["Фамилия", "Имя", "Телефон", "Описание"]
+    with open(filename, 'r', encoding='utf-8') as phb:
+        for line in phb:
+            values = line.strip().split(',')
+            if len(values) == len(fields):
+                record = dict(zip(fields, values))
+                phone_book.append(record)
+            else:
+                print(f"Ошибка в строке: {line.strip()}")
+    return phone_book
+
+def write_txt(filename, phone_book):
+    with open(filename, 'w', encoding='utf-8') as phout:
+        for record in phone_book:
+            s = ",".join(record.values())
+            phout.write(f"{s}\n")
+
+def print_result(phone_book):
+    for record in phone_book:
+        print(record)
+
+def find_by_lastname(phone_book, last_name):
+    result = [record for record in phone_book if record.get("Фамилия") == last_name]
+    return result if result else "Абонент не найден"
+
+def find_by_number(phone_book, number):
+    result = [record for record in phone_book if record.get("Телефон") == number]
+    return result if result else "Номер не найден"
+
+def add_user(phone_book, user_data):
+    fields = ["Фамилия", "Имя", "Телефон", "Описание"]
+    values = user_data.split(',')
+    if len(values) == len(fields):
+        record = dict(zip(fields, values))
+        phone_book.append(record)
+        print("Абонент добавлен")
+    else:
+        print("Ошибка: Неправильный формат данных")
+
+work_with_phonebook()
